483596e6f238a7de96d3f55aac26552f4fd618d1
Aris Ripandi <riipandi@gmail.com>
2024-10-25 22:10:59 +0700

    feat: add CLI example command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export default {}
+import { defineCommand, runMain } from 'citty'
+
+const main = defineCommand({
+  meta: {
+    name: 'nitrolux',
+    version: '0.0.1',
+    description: 'Nitrolux Command Line Interface',
+  },
+  args: {
+    name: {
+      type: 'positional',
+      description: 'Application name',
+      required: true,
+    },
+    friendly: {
+      type: 'boolean',
+      description: 'Use friendly greeting',
+      alias: 'F',
+    },
+  },
+  run({ args }) {
+    console.info(`${args.friendly ? 'Howdy,' : 'Creating application:'} ${args.name}`)
+  },
+})
+
+runMain(main)
